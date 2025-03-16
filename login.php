6f14441d6e9f1f7b08da9ca0b6d4cfe94aab922f
chonlachit <chonlachit2233@gmail.com>
2025-03-16 19:54:29 +0700

    ตกเเต่งlogin-logout

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ error_reporting(0);
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="styless.css">
+  <link rel="stylesheet" href="stylessl.css">
 </head>
 <body>
 
--- signup.php
+++ signup.php
@@ -50,7 +50,7 @@ if (isset($_POST['signup'])) {
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="stylessingup.css">
+  <link rel="stylesheet" href="stysingup.css">
 </head>
 <body>
 
